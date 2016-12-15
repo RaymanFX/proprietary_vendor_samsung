a46c34002e0545f4ab8450726461c7fa31e21a68
Christopher N. Hesse <raymanfx@gmail.com>
2016-12-15 23:03:26 +0100

    noblelte: Blob out audio
    Until we figure out how to fix incall audio with the OSS
    audio stack.

--- noblelte-common/noblelte-common-vendor-blobs.mk
+++ noblelte-common/noblelte-common-vendor-blobs.mk
@@ -59,6 +59,42 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/noblelte-common/proprietary/vendor/firmware/companion_fw_imx240_evt1.bin:system/vendor/firmware/companion_fw_imx240_evt1.bin \
     vendor/samsung/noblelte-common/proprietary/vendor/firmware/companion_imx240_master_setfile.bin:system/vendor/firmware/companion_imx240_master_setfile.bin \
     vendor/samsung/noblelte-common/proprietary/vendor/firmware/companion_imx240_mode_setfile.bin:system/vendor/firmware/companion_imx240_mode_setfile.bin \
+    vendor/samsung/noblelte-common/proprietary/lib/hw/audio.primary.universal7420.so:system/lib/hw/audio.primary.universal7420.so \
+    vendor/samsung/noblelte-common/proprietary/lib/lib_DNSe_EP_ver216c.so:system/lib/lib_DNSe_EP_ver216c.so \
+    vendor/samsung/noblelte-common/proprietary/lib/lib_DNSe_NRSS_ver226.so:system/lib/lib_DNSe_NRSS_ver226.so \
+    vendor/samsung/noblelte-common/proprietary/lib/lib_SA_GoogleFX_ver124b.so:system/lib/lib_SA_GoogleFX_ver124b.so \
+    vendor/samsung/noblelte-common/proprietary/lib/lib_SamsungRec_V04013_WNR004.so:system/lib/lib_SamsungRec_V04013_WNR004.so \
+    vendor/samsung/noblelte-common/proprietary/lib/lib_SamsungVAD_v01009.so:system/lib/lib_SamsungVAD_v01009.so \
+    vendor/samsung/noblelte-common/proprietary/lib/lib_Samsung_Resampler.so:system/lib/lib_Samsung_Resampler.so \
+    vendor/samsung/noblelte-common/proprietary/lib/lib_soundaliveresampler.so:system/lib/lib_soundaliveresampler.so \
+    vendor/samsung/noblelte-common/proprietary/lib/lib_SoundAlive_3DPosition_ver107.so:system/lib/lib_SoundAlive_3DPosition_ver107.so \
+    vendor/samsung/noblelte-common/proprietary/lib/lib_SoundAlive_3DRendering_ver102.so:system/lib/lib_SoundAlive_3DRendering_ver102.so \
+    vendor/samsung/noblelte-common/proprietary/lib/lib_SoundAlive_AlbumArt_ver101.so:system/lib/lib_SoundAlive_AlbumArt_ver101.so \
+    vendor/samsung/noblelte-common/proprietary/lib/lib_SoundAlive_play_plus_hal_ver126b.so:system/lib/lib_SoundAlive_play_plus_hal_ver126b.so \
+    vendor/samsung/noblelte-common/proprietary/lib/lib_SoundAlive_play_plus_ver126b.so:system/lib/lib_SoundAlive_play_plus_ver126b.so \
+    vendor/samsung/noblelte-common/proprietary/lib/lib_SoundAlive_SRC192_ver205.so:system/lib/lib_SoundAlive_SRC192_ver205.so \
+    vendor/samsung/noblelte-common/proprietary/lib/lib_SoundAlive_SRC20k_ver100.so:system/lib/lib_SoundAlive_SRC20k_ver100.so \
+    vendor/samsung/noblelte-common/proprietary/lib/lib_SoundAlive_UHD_Audio.so:system/lib/lib_SoundAlive_UHD_Audio.so \
+    vendor/samsung/noblelte-common/proprietary/lib/lib_SoundAlive_ver118t.so:system/lib/lib_SoundAlive_ver118t.so \
+    vendor/samsung/noblelte-common/proprietary/lib/lib_SoundBooster_hal_ver609c.so:system/lib/lib_SoundBooster_hal_ver609c.so \
+    vendor/samsung/noblelte-common/proprietary/lib/lib_SoundBooster_ver610.so:system/lib/lib_SoundBooster_ver610.so \
+    vendor/samsung/noblelte-common/proprietary/lib/libaudio-ril.so:system/lib/libaudio-ril.so \
+    vendor/samsung/noblelte-common/proprietary/lib/libaudioroute.so:system/lib/libaudioroute.so \
+    vendor/samsung/noblelte-common/proprietary/lib/libLifevibes_lvazfs.so:system/lib/libLifevibes_lvazfs.so \
+    vendor/samsung/noblelte-common/proprietary/lib/libpreprocessing_nxp.so:system/lib/libpreprocessing_nxp.so \
+    vendor/samsung/noblelte-common/proprietary/lib/libsamsungDiamondVoiceExtSwb.so:system/lib/libsamsungDiamondVoiceExtSwb.so \
+    vendor/samsung/noblelte-common/proprietary/lib/libSamsungPostProcess.so:system/lib/libSamsungPostProcess.so \
+    vendor/samsung/noblelte-common/proprietary/lib/libSamsungPostProcessConvertor.so:system/lib/libSamsungPostProcessConvertor.so \
+    vendor/samsung/noblelte-common/proprietary/lib/libSamsungPreProcess.so:system/lib/libSamsungPreProcess.so \
+    vendor/samsung/noblelte-common/proprietary/lib/libsamsungRecord_ns.so:system/lib/libsamsungRecord_ns.so \
+    vendor/samsung/noblelte-common/proprietary/lib/libsamsungRecord.so:system/lib/libsamsungRecord.so \
+    vendor/samsung/noblelte-common/proprietary/lib/libsamsungSoundbooster_ext.so:system/lib/libsamsungSoundbooster_ext.so \
+    vendor/samsung/noblelte-common/proprietary/lib/libsecaudiocoreutils.so:system/lib/libsecaudiocoreutils.so \
+    vendor/samsung/noblelte-common/proprietary/lib/libsecril-client.so:system/lib/libsecril-client.so \
+    vendor/samsung/noblelte-common/proprietary/lib/libtinyalsa.so:system/lib/libtinyalsa.so \
+    vendor/samsung/noblelte-common/proprietary/lib/libtinycompress.so:system/lib/libtinycompress.so \
+    vendor/samsung/noblelte-common/proprietary/lib/libtwomicinterface.so:system/lib/libtwomicinterface.so \
+    vendor/samsung/noblelte-common/proprietary/lib/libunshorten.so:system/lib/libunshorten.so \
     vendor/samsung/noblelte-common/proprietary/etc/firmware/audience-es804-data.bin:system/etc/firmware/audience-es804-data.bin \
     vendor/samsung/noblelte-common/proprietary/etc/firmware/audience-es804-fw.bin:system/etc/firmware/audience-es804-fw.bin \
     vendor/samsung/noblelte-common/proprietary/etc/firmware/clearwater-dsp3-tx-nb.bin:system/etc/firmware/clearwater-dsp3-tx-nb.bin \
